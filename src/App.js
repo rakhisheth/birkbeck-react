3cc43dd340b4a05bcbe566c759ea15dbff7102c8
Rakhi Sheth <rakhi.sheth@outlook.com>
2018-10-02 17:00:36 +0100

    add navbar and css styling

--- src/App.js
+++ src/App.js
@@ -1,18 +1,27 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
+import Title from "./Components/title";
+import Links from "./Components/links";
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <div className="navbar">
+          <header className="App-header">
+            <img src="http://wdux1819.bbkweb.org/img/bbk-logo-burgundy.png" />
+          </header>
+          <Title title="Web Development & User Experience" />
+          <ul className="nav-links">
+            <Links url="" name="Home" />
+            <Links url="" name="Assignment" />
+            <Links url="" name="Mailing List" />
+            <Links url="" name="Reading List" />
+            <Links url="" name="Sessions" />
+            <Links url="" name="Staff" />
+            <Links url="" name="Students" />
+          </ul>
+        </div>
       </div>
     );
   }
---
+++ src/Components/links.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Links = props => {
+  return (
+    <li>
+      {" "}
+      <a href={props.url}>{props.name}</a>
+    </li>
+  );
+};
+
+export default Links;
